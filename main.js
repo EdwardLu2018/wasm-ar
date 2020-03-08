86ff7f208e57f5e280cca5ea1eb06b10e0605f56
Edward <lued2018@gmail.com>
2020-03-08 00:12:19 -0600

    Update main.js

--- main.js
+++ main.js
@@ -101,13 +101,13 @@ function processVideo() {
             cv.drawMatches(src_gray, kp1, ref_img, kp2, good, dst);
             // cv.drawKeypoints(ref_img, kp2, dst);
 
-            console.log(good.get(0))
             let coords1 = []
             let coords2 = []
             for (let i = 0; i < good.size(); i++) {
                 let m = good.get(i);
                 coords1.push([kp1.get(m.queryIdx).pt]);
                 coords2.push([kp2.get(m.queryIdx).pt]);
+                console.log(kp1.get(m.queryIdx).pt)
             }
             const rows = good.size(), cols = 2;
             let coords1_mat = cv.matFromArray(rows, cols, cv.CV_8UC1, coords1);
