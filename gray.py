bca8af73e55971b0581382bbe429302cd3920308
Edward <lued2018@gmail.com>
2020-03-08 17:46:31 -0500

    attempt to fix channel mismatch

--- gray.py
+++ gray.py
@@ -1,5 +1,5 @@
 import cv2
 
-im = cv2.imread("./ref.jpg")
+im = cv2.imread("./hp_cover.jpg")
 gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
-cv2.imwrite("./ref_gray.jpg", gray)
+cv2.imwrite("./hp_gray.jpg", gray)
