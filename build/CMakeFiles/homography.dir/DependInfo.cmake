8927ce719b2b52bd9fadebc69037b0723bb5dbd6
Edward <lued2018@gmail.com>
2020-05-08 23:47:48 -0500

    add build

---
+++ build/CMakeFiles/homography.dir/DependInfo.cmake
@@ -0,0 +1,94 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/Edward/Documents/GitHub/wasm-ar/homography.cpp" "/Users/Edward/Documents/GitHub/wasm-ar/build/CMakeFiles/homography.dir/homography.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../opencv/include"
+  "../opencv/build_wasm"
+  "../opencv/modules/calib3d/include"
+  "../opencv/modules/core/include"
+  "../opencv/modules/dnn/include"
+  "../opencv/modules/features2d/include"
+  "../opencv/modules/flann/include"
+  "../opencv/modules/gapi/include"
+  "../opencv/modules/highgui/include"
+  "../opencv/modules/imgcodecs/include"
+  "../opencv/modules/imgproc/include"
+  "../opencv/modules/ml/include"
+  "../opencv/modules/objdetect/include"
+  "../opencv/modules/photo/include"
+  "../opencv/modules/stitching/include"
+  "../opencv/modules/ts/include"
+  "../opencv/modules/video/include"
+  "../opencv/modules/videoio/include"
+  "../opencv/modules/world/include"
+  "../opencv_contrib/modules/alphamat/include"
+  "../opencv_contrib/modules/aruco/include"
+  "../opencv_contrib/modules/bgsegm/include"
+  "../opencv_contrib/modules/bioinspired/include"
+  "../opencv_contrib/modules/ccalib/include"
+  "../opencv_contrib/modules/cnn_3dobj/include"
+  "../opencv_contrib/modules/cudaarithm/include"
+  "../opencv_contrib/modules/cudabgsegm/include"
+  "../opencv_contrib/modules/cudacodec/include"
+  "../opencv_contrib/modules/cudafeatures2d/include"
+  "../opencv_contrib/modules/cudafilters/include"
+  "../opencv_contrib/modules/cudaimgproc/include"
+  "../opencv_contrib/modules/cudalegacy/include"
+  "../opencv_contrib/modules/cudaobjdetect/include"
+  "../opencv_contrib/modules/cudaoptflow/include"
+  "../opencv_contrib/modules/cudastereo/include"
+  "../opencv_contrib/modules/cudawarping/include"
+  "../opencv_contrib/modules/cudev/include"
+  "../opencv_contrib/modules/cvv/include"
+  "../opencv_contrib/modules/datasets/include"
+  "../opencv_contrib/modules/dnn_objdetect/include"
+  "../opencv_contrib/modules/dnn_superres/include"
+  "../opencv_contrib/modules/dpm/include"
+  "../opencv_contrib/modules/face/include"
+  "../opencv_contrib/modules/freetype/include"
+  "../opencv_contrib/modules/fuzzy/include"
+  "../opencv_contrib/modules/hdf/include"
+  "../opencv_contrib/modules/hfs/include"
+  "../opencv_contrib/modules/img_hash/include"
+  "../opencv_contrib/modules/intensity_transform/include"
+  "../opencv_contrib/modules/line_descriptor/include"
+  "../opencv_contrib/modules/matlab/include"
+  "../opencv_contrib/modules/optflow/include"
+  "../opencv_contrib/modules/ovis/include"
+  "../opencv_contrib/modules/phase_unwrapping/include"
+  "../opencv_contrib/modules/plot/include"
+  "../opencv_contrib/modules/quality/include"
+  "../opencv_contrib/modules/rapid/include"
+  "../opencv_contrib/modules/reg/include"
+  "../opencv_contrib/modules/rgbd/include"
+  "../opencv_contrib/modules/saliency/include"
+  "../opencv_contrib/modules/sfm/include"
+  "../opencv_contrib/modules/shape/include"
+  "../opencv_contrib/modules/stereo/include"
+  "../opencv_contrib/modules/structured_light/include"
+  "../opencv_contrib/modules/superres/include"
+  "../opencv_contrib/modules/surface_matching/include"
+  "../opencv_contrib/modules/text/include"
+  "../opencv_contrib/modules/tracking/include"
+  "../opencv_contrib/modules/videostab/include"
+  "../opencv_contrib/modules/viz/include"
+  "../opencv_contrib/modules/xfeatures2d/include"
+  "../opencv_contrib/modules/ximgproc/include"
+  "../opencv_contrib/modules/xobjdetect/include"
+  "../opencv_contrib/modules/xphoto/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
